366b66859d1c4d2dae1d54282ddf4c930a6504ec
Q.Dũng <110549573+Blank129@users.noreply.github.com>
2023-11-16 23:32:06 +0700

    Add files via upload

---
+++ web-java/src/main/java/com/laptrinhjavaweb/service/impl/NewService.java
@@ -0,0 +1,58 @@
+package com.laptrinhjavaweb.service.impl;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import com.laptrinhjavaweb.dao.INewDAO;
+import com.laptrinhjavaweb.model.NewModel;
+import com.laptrinhjavaweb.paging.Pageble;
+import com.laptrinhjavaweb.service.INewService;
+
+public class NewService implements INewService{
+	@Inject
+	private INewDAO newDao;
+	public List<NewModel> findByCategoryId(Long CategoryId){
+		return newDao.findByCategoryId(CategoryId);
+	}
+	
+	@Override
+	public NewModel save(NewModel newModel) {
+		newModel.setCreatedDate(new Timestamp(System.currentTimeMillis()));
+		newModel.setCreatedBy("");
+		Long newId = newDao.save(newModel);
+		return newDao.findOne(newId);
+	}
+
+	@Override
+	public NewModel update(NewModel updateNew) {
+		NewModel oldNew = newDao.findOne(updateNew.getId());
+		updateNew.setCreatedDate(oldNew.getCreatedDate());
+		updateNew.setCreatedBy(oldNew.getCreatedBy());
+		updateNew.setModifiedDate(new Timestamp(System.currentTimeMillis()));
+		updateNew.setModifiedBy("");
+		newDao.update(updateNew);
+		return newDao.findOne(updateNew.getId());
+	}
+
+	@Override
+	public void delete(long[] ids) {
+		for(long id: ids) {
+			newDao.delete(id);
+		}
+		
+	}
+
+	@Override
+	public List<NewModel> findAll(Pageble pageble) {
+		return newDao.findAll(pageble);
+	}
+
+	@Override
+	public int getTotalItem() {
+		// TODO Auto-generated method stub
+		return newDao.getTotalItem();
+	}
+
+}
